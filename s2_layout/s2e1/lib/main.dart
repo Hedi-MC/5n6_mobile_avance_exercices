f77d4b45cd21f1ee3e47da3188eeaa6ad7cb5025
Hedi Mélo Cherni <2030719@cegepmontpetit.ca>
2024-08-28 16:32:50 -0400

    s2e1 Drapeau fini

--- s2_layout/s2e1/lib/main.dart
+++ s2_layout/s2e1/lib/main.dart
@@ -86,39 +86,47 @@ class _MyHomePageState extends State<MyHomePage> {
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      body: Row(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Expanded(
+            flex: 1,
+            child: Container(
+              // Ca prend un container pour les bordures et les marges
+              width: double.infinity,
+
+              color: Colors.green,
+              child: const Padding(
+                // Le padding se fait en encapsulant dans le widget Padding
+                padding: EdgeInsets.all(8.0),
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+          ),
+          Expanded(
+            flex: 1,
+            child: Container(
+              // Ca prend un container pour les bordures et les marges
+              width: double.infinity,
+              color: Colors.white,
+              child: const Padding(
+                // Le padding se fait en encapsulant dans le widget Padding
+                padding: EdgeInsets.all(8.0),
+              ),
             ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+          ),
+          Expanded(
+            flex: 1,
+            child: Container(
+              // Ca prend un container pour les bordures et les marges
+              width: double.infinity,
+              color: Colors.red,
+              child: const Padding(
+                // Le padding se fait en encapsulant dans le widget Padding
+                padding: EdgeInsets.all(8.0),
+              ),
+            ),
+          ),
+        ],
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
